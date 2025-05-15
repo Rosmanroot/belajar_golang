2c762bd0e140ebc7f41271e911b0f7195e223d2d
Nurfanisrosman <147623166+Rosmanroot@users.noreply.github.com>
2025-05-15 21:06:12 +0700

    tes

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error{
-		return c.SendString("Hello, Guys")
+		return c.SendString("Haii, Teman, Teman")
 	})
 
 	app.Listen(":3000")
