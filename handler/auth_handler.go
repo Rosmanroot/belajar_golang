a7e126d18f0bf7b0107eed5c5fa0628f3070202f
Nurfanisrosman <147623166+Rosmanroot@users.noreply.github.com>
2025-05-26 17:28:23 +0700

    tes

--- handler/auth_handler.go
+++ handler/auth_handler.go
@@ -1,1 +1,55 @@
 package handler
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"golang2/model"
+	"golang2/service"
+)
+
+// Handler untuk login
+func LoginHandler(c *fiber.Ctx) error {
+	var input model.User
+
+	if err := c.BodyParser(&input); err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"message": "Format JSON tidak valid",
+		})
+	}
+
+	success, msg := service.Login(input)
+
+	if success {
+		return c.JSON(fiber.Map{
+			"message": msg,
+			"user":    input.Username,
+		})
+	}
+
+	return c.Status(401).JSON(fiber.Map{
+		"message": msg,
+	})
+}
+
+// Handler untuk register
+func RegisterHandler(c *fiber.Ctx) error {
+	var input model.User
+
+	if err := c.BodyParser(&input); err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"message": "Format JSON tidak valid",
+		})
+	}
+
+	success, msg := service.Register(input)
+
+	if success {
+		return c.JSON(fiber.Map{
+			"message": msg,
+			"user":    input.Username,
+		})
+	}
+
+	return c.Status(409).JSON(fiber.Map{
+		"message": msg,
+	})
+}
