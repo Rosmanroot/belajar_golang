43267b709e7db171ddbd00b24a7b3d095ff4dc30
Nurfanisrosman <147623166+Rosmanroot@users.noreply.github.com>
2025-05-26 17:29:15 +0700

    tes

--- service/auth_service.go
+++ service/auth_service.go
@@ -1,1 +1,29 @@
 package service
+
+import (
+	"golang2/model"
+)
+
+var users []model.User
+
+// Register user baru
+func Register(user model.User) (bool, string) {
+	// Cek jika username sudah ada
+	for _, u := range users {
+		if u.Username == user.Username {
+			return false, "Username sudah digunakan"
+		}
+	}
+	users = append(users, user)
+	return true, "Registrasi berhasil"
+}
+
+// Login user
+func Login(user model.User) (bool, string) {
+	for _, u := range users {
+		if u.Username == user.Username && u.Password == user.Password {
+			return true, "Login berhasil"
+		}
+	}
+	return false, "Username atau password salah"
+}
